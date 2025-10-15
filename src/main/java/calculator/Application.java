7579cda434bc6e0163ef613643c2864903364678
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-10-15 17:25:05 +0900

    refactor(): Clean Code 준수를 위한 코드 리팩터링
    - Clean Code를 지키기 위해 메서드 분리
    - main 메서드 로직 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -22,73 +22,12 @@ public class Application {
             return;
         }
 
-        // 커스텀 구분자를 찾는 정규표현식 패턴
-        Pattern pattern = Pattern.compile("^//(.)(?:\\\\n)");
-
-        // 커스텀 구분자 추출
-        Matcher matcher = pattern.matcher(textLine);
-
-        // 커스텀 구분자 존재하는 지 판별
-        if (matcher.find()) {
-
-            // 커스텀 구분자를 담을 변수 생성
-            String customSeparator = matcher.group(1);
-
-            // matcher.end() → //X\n 뒤의 실제 숫자 구간만 남기기
-            textLine = textLine.substring(matcher.end());
-
-            // 숫자, 기본 구분자(, :), 커스텀 구분자 정규식
-            String regex =
-                    "^[0-9,:" + Pattern.quote(customSeparator) + "]*$";
-
-            // 숫자, 기본 구분자(, :), 커스텀 구분자만 포함하는지 검사
-            if (textLine.matches(regex)) {
-
-                // 입력 문자열 내의 ":" 또는 커스텀 구분자를 ","로 통일
-                textLine = textLine.replaceAll(":|" +
-                        Pattern.quote(customSeparator), ",");
-
-                // 쉼표(,) 기준으로 숫자 분리
-                String[] parts = textLine.split(",");
-
-                // 각 숫자를 합산
-                int result = 0;
-                for (String part : parts) {
-                    result += Integer.parseInt(part);
-                }
-
-                // 최종 결과 출력
-                System.out.printf("결과 : %d", result);
-
-            } else {
-                // 잘못된 입력(허용되지 않은 문자 포함 시) 예외 처리
-                handleInvalidInput();
-            }
-
-        } else { // 커스텀 구분자가 존재하지 않는 경우
-
-            // 숫자, 쉼표(,), 콜론(:)만 허용하는 정규식
-            String regex = "^[0-9,:]*$";
-            if (textLine.matches(regex)) {
-                // ":"를 ","로 변환 (기본 구분자 통일)
-                textLine = textLine.replaceAll(":", ",");
-
-                // 쉼표(,) 기준으로 숫자 분리
-                String[] parts = textLine.split(",");
-
-                // 각 숫자를 합산
-                int result = 0;
-                for (String part : parts) {
-                    result += Integer.parseInt(part);
-                }
-
-                // 최종 결과 출력
-                System.out.printf("결과 : %d", result);
-            } else {
-                // 잘못된 입력(허용되지 않은 문자 포함 시) 예외 처리
-                handleInvalidInput();
-            }
+        if (hasCustomSeparator(textLine)) {
+            handleCustomSeparator(textLine);
+            return;
         }
+
+        handleDefaultSeparator(textLine);
     }
 
     // 입력이 비어있는지 확인
