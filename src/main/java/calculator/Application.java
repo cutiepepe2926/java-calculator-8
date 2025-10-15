8b3f74bfb03de34b375b0389397890f77944d06c
cutiepepe2926 <cutiepepe2926@gmail.com>
2025-10-15 14:45:45 +0900

    feat: 커스텀 구분자 탐색 기능 구현
    - 정규표현식을 사용해 입력 문자열에서 커스텀 구분자 패턴 탐색
    - `//`와 `\n` 사이의 구분자를 추출하도록 구현
    - 커스텀 구분자 존재 여부를 판별하는 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,9 @@ package calculator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -9,6 +12,20 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String textLine = readLine(); //camp.nextstep.edu.missionutils.Console의 static 메서드 readLine()
 
+        // 커스텀 구분자를 찾는 정규표현식 패턴
+        Pattern pattern = Pattern.compile("^//(.)(?:\\\\n)");
+
+        // 커스텀 구분자 추출
+        Matcher matcher = pattern.matcher(textLine);
+
+        // 커스텀 구분자 존재하는 지 판별
+        if(matcher.find()){
+            System.out.println("커스텀 구분자 존재 O");
+        }
+        else {
+            System.out.println("커스텀 구분자 존재 X");
+
+        }
 
 
     }
